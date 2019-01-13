63aedae9812229254a450f03020d785064a49b65
Shingirai Chanakira <sechanakira@yahoo.com>
2019-01-13 15:58:24 +0200

    Initial Commit

---
+++ src/main/java/com/github/sechanakira/ratebot/RateBotApplication.java
@@ -0,0 +1,14 @@
+package com.github.sechanakira.ratebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateBotApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/github/sechanakira/ratebot/RateBotApplicationTests.java
@@ -0,0 +1,17 @@
+package com.github.sechanakira.ratebot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RateBotApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
